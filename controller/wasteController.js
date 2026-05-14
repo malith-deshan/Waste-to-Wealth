b9757cfc12ff85e64ecd39962bccbc1c8a45a313
malith-deshan <mdpemasiri456@gmail.com>
2026-05-14 22:47:42 +0530

    implement all controllers

--- controller/wasteController.js
+++ controller/wasteController.js
@@ -10,3 +10,46 @@ export const create = async(req, res) => {
         res.status(500).json({error: "Internal Server Error."});
     }
 };
+
+// READ - GET ALL
+export const fetch = async (req, res) => {
+    try {
+        const reports = await Waste.find();
+        if(reports.length === 0) {
+            return res.status(404).json({message: "No reports found."});
+        }
+        res.status(200).json(reports);
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error."});
+    }
+};
+
+// UPDATE - PUT
+export const update = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const reportExist = await Waste.findOne({_id:id});
+        if (!reportExist) {
+            return res.status(404).json({message: "Report not found."});
+        }
+        const updatedReport = await Waste.findByIdAndUpdate(id, req.body, {new: true});
+        res.status(201).json(updatedReport);
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error."});
+    }
+};
+
+// DELETE
+export const deleteReport = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const reportExist = await Waste.findOne({_id:id});
+        if(!reportExist) {
+            return res.status(404).json({message: "Report not found."});
+        }
+        await Waste.findByIdAndDelete(id);
+        res.status(201).json({message: "Report deleted successfully."});
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error."});
+    }
+};
